efaaaa7d90dbf1720d9718a04d364334850c6981
KhuePM <minhkhuepham99@gmail.com>
2022-11-13 20:50:35 +0700

    Initial Commit

---
+++ CounterMVVM/CounterMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterMVVMApp.swift
+//  CounterMVVM
+//
+//  Created by Phạm Minh Khuê on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
